140fda20c135c0b14b2c0b17706d02a3a0f30fae
ManfredAabye <zainhofer@gmx.de>
2024-12-24 12:40:43 +0100

    24122024

--- start.bat
+++ start.bat
@@ -1,12 +1,30 @@
 @echo off
-:: Verzeichnis definieren
+
+echo _____________________________________________________________________________________________________________________
+echo opensimMULTITOOL Starter - Starte OpenSimulator...
+echo _____________________________________________________________________________________________________________________
+
 set "binDir=opensimsource\bin"
+set "mariaDir=opensimsource\OpenSim-MariaDB-Micro-Database\DATABASE\bin"
+
+:: Prüfen, ob OpenSim-MariaDB-Micro-Database existiert und starten
+if exist "opensimsource\OpenSim-MariaDB-Micro-Database\DATABASE\bin\mariastart.bat" (
+    echo mariastart.bat gefunden. Starte Database...
+    pushd "%mariaDir%"
+    start "" mariastart.bat
+    popd
+) else (
+    echo mariastart.bat nicht gefunden.
+)
+
+:: Warten, um sicherzustellen, dass der Server bereit ist
+timeout /t 5 > nul
 
 :: Prüfen, ob MoneyServer.dll existiert und starten
 if exist "%binDir%\MoneyServer.dll" (
     echo MoneyServer.dll gefunden. Starte MoneyServer...
     pushd "%binDir%"
-    dotnet MoneyServer.dll
+    start "" dotnet MoneyServer.dll
     popd
 ) else (
     echo MoneyServer.dll nicht gefunden.
@@ -16,11 +34,10 @@ if exist "%binDir%\MoneyServer.dll" (
 if exist "%binDir%\OpenSim.dll" (
     echo OpenSim.dll gefunden. Starte OpenSim...
     pushd "%binDir%"
-    dotnet OpenSim.dll
+    start "" dotnet OpenSim.dll
     popd
 ) else (
     echo OpenSim.dll nicht gefunden.
 )
 
-:: Skript abgeschlossen
 echo Alle Aufgaben abgeschlossen.
